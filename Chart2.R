4632bc03afe6e8fba01bb66a531847c8cdef9729
bonche22 <bonche22@uw.edu>
2023-02-23 17:18:31 -0800

    adding new changes

--- Chart2.R
+++ Chart2.R
@@ -20,7 +20,16 @@ book_data <- book_data %>%
   mutate(summarise_title3 = gsub("III .*","", summarise_title2))
 
 book_data <- book_data %>%
-  mutate(Title = gsub("\\:.*","", summarise_title3))
+  mutate(summarise_title4 = gsub("\\:.*","", summarise_title3))
+
+book_data <- book_data %>%
+  mutate(summarise_title5 = gsub("\\ I.*","", summarise_title4))
+
+book_data <- book_data %>%
+  mutate(summarise_title6 = gsub("\\of Ernest Hemingway.*","", summarise_title5))
+
+book_data <- book_data %>%
+  mutate(Title = gsub("\\ Volume.*","", summarise_title6))
 
 book_data <- book_data %>%
   group_by(Title, year) %>%
@@ -28,4 +37,5 @@ book_data <- book_data %>%
 
 bar_chart <- ggplot(book_data, aes(x = year, y = total_checkouts)) +
   geom_bar(stat = "identity", aes(fill = Title)) +
-  labs(title = "Ernest Hemingway's Book Checkouts by Year", x = "Year", y = "Amount of Checkouts")
+  labs(title = "Ernest Hemingway's Book Checkouts by Year", x = "Year", y = "Amount of Checkouts") +
+  theme(legend.text = element_text(size = 5))   
